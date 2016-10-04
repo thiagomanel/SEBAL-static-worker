c3ad7e15d80a69aae95b8320ed47e7ba6f981b51
EsdrasVP <esdras015@gmail.com>
2016-10-04 14:23:21 -0300

    Fixing permission problem with log file

--- bin/init.sh
+++ bin/init.sh
@@ -4,4 +4,7 @@
 WORKER_LOG_FILE=/var/log/sebal_static_worker.log
 WORKER_LOG_INIT_MESSAGE="$(date):: Initializing SEBAL nope worker"
 
-sudo echo "$WORKER_LOG_INIT_MESSAGE" >> "$WORKER_LOG_FILE"
+sudo touch $WORKER_LOG_FILE
+sudo chmod 777 $WORKER_LOG_FILE
+
+echo "$WORKER_LOG_INIT_MESSAGE" >> $WORKER_LOG_FILE
